e475ae74410265c94cc8319c6065a1fc77e5fdbd
Aleksandr <Lunatik_@bk.ru>
2017-04-04 12:56:15 +0500

    first commit

---
+++ src/com/company/ArrList.java
@@ -0,0 +1,22 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+/**
+ * Created by admin on 04.04.2017.
+ */
+public class ArrList<T extends Number> {
+    double[] arr;
+
+    public void add(T a) {
+        arr[arr.length] = a;
+    }
+
+    public void remove() {
+        //
+    }
+
+    public void finde(T a) {
+        //
+    }
+}
