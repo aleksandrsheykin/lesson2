4e9a104efd203fd4f8daf37768860365444263e7
Aleksandr <Lunatik_@bk.ru>
2017-04-04 18:44:09 +0500

    add methods get and set

--- src/com/company/ArrList.java
+++ src/com/company/ArrList.java
@@ -26,17 +26,27 @@ public class ArrList<T> {
         tmp[size++] = a;
         arr = tmp;
     }
-
+    
     public void add(T a, int ind) {
         if(ind<0) return;
-        if(ind>=size) {
+
+        if(ind>=size) {     //вставляем в конец или за край массива
             T[] tmp = (T[]) new Object[ind+1];
             for (int i=0; i<size; i++)
                 tmp[i] = arr[i];
             size = ind+1;
             arr = tmp;
+            arr[ind] = a;
+        } else {    //вставляем в середину или начало массива
+            T[] tmp = (T[]) new Object[size+1];
+            for (int i=0; i<ind; i++)
+                tmp[i] = arr[i];
+            for (int i=ind; i<size; i++)
+                tmp[i+1] = arr[i];
+            size++;
+            arr = tmp;
+            arr[ind] = a;
         }
-        arr[ind] = a;
     }
 
     public void remove(int ind) {
@@ -57,6 +67,22 @@ public class ArrList<T> {
             if(arr[i] == a) return i;
         }
         return -1;
+    }
+
+    public int set(T a, int ind) {
+        if(ind<0) return -1;
+
+        if(ind>size) {
+            this.add(a, ind);
+        } else {
+            arr[ind] = a;
+        }
+        return ind;
+    }
+
+    public T get(int ind) {
+        if (ind<0 || ind>size) return null;
+        return arr[ind];
     }
 
     public void writeList() {
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,16 +3,24 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        ArrList<String> arr = new ArrList<>(10);
+        ArrList<String> arr = new ArrList<>(5);
+        arr.writeList();
         arr.add("aaa");
+        arr.writeList();
         arr.add("bbb", 1);
-        arr.add("ccc", 5);
-        arr.add("ddd", 11);
+        arr.writeList();
+        arr.add("ccc", 4);
+        arr.writeList();
+        arr.add("ddd", 16);
         arr.writeList();
         arr.remove(5);
+        arr.remove(4);
         arr.writeList();
         System.out.println("indexOf('bbb') = " + arr.indexOf("bbb"));
         System.out.println("indexOf('asdf') = " + arr.indexOf("asdf"));
+        arr.set("fff", 3);
+        System.out.println("arr.get(3) = " + arr.get(3));
+
 
 
     }
