aceb3172ab723f1d8d92d36222221845133ea26e
Aleksandr <Lunatik_@bk.ru>
2017-04-04 16:32:38 +0500

    aarggh commit

--- src/com/company/ArrList.java
+++ src/com/company/ArrList.java
@@ -5,18 +5,37 @@ import java.lang.reflect.Array;
 /**
  * Created by admin on 04.04.2017.
  */
-public class ArrList<T extends Number> {
-    double[] arr;
+public class ArrList<T> {
+    private int size = 0;
+    //private <T> arr[];
+    //<T>[] arr = new Array(1);
+    //private Double[] arr;
+    //private <T>[] arr;
+    //private Object<T> arr = new Object<>();
+    //private Object arr;
+    private Array arr [];
+
+    ArrList() {
+        arr = new Array[0];
+    }
+
+    ArrList(int capacity) {
+        arr = new Array[capacity];
+    }
 
     public void add(T a) {
-        arr[arr.length] = (double) a;
+        arr[0] = a;
     }
 
     public void remove(int ind) {
         //arr[ind].remove;
     }
 
-    public void finde(T a) {
+    public void find(T a) {
         //
+    }
+
+    public void writeList() {
+        System.out.println(arr.toString());
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,8 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        ArrList<Double> arr = new ArrList<>();
-
+        ArrList<Double> arr = new ArrList<>(10);
+        arr.add(12.0);
+        System.out.println(arr.toString());
     }
 }
