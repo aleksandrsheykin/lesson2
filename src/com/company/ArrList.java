bb4cdadef72689e17fa7b166ebf525da60460b8c
Aleksandr <Lunatik_@bk.ru>
2017-04-04 17:57:45 +0500

    adding indexOf method

--- src/com/company/ArrList.java
+++ src/com/company/ArrList.java
@@ -14,33 +14,54 @@ public class ArrList<T> {
     }
 
     ArrList(int capacity) {
-        arr = (T[]) new Object[capacity];
+        if(capacity<0) return;
+        size = capacity;
+        arr = (T[]) new Object[size];
     }
 
     public void add(T a) {
-        arr[size++] = a;
+        T[] tmp = (T[]) new Object[size+1];
+        for (int i=0; i<size; i++)
+            tmp[i] = arr[i];
+        tmp[size++] = a;
+        arr = tmp;
     }
 
     public void add(T a, int ind) {
-        if(ind>size) {
-            size = ind;
+        if(ind<0) return;
+        if(ind>=size) {
             T[] tmp = (T[]) new Object[ind+1];
-            
-        } else {
-            arr[ind] = a;
+            for (int i=0; i<size; i++)
+                tmp[i] = arr[i];
+            size = ind+1;
+            arr = tmp;
         }
+        arr[ind] = a;
     }
 
     public void remove(int ind) {
-        //arr[ind].remove;
+        if(ind>size) return;
+        if(ind<0) return;
+
+        T[] tmp = (T[]) new Object[size-1];
+        for (int i=0; i<ind; i++)
+            tmp[i] = arr[i];
+        for (int i=ind+1; i<size; i++)
+            tmp[i-1] = arr[i];
+        size--;
+        arr = tmp;
     }
 
-    public void find(T a) {
-        //
+    public int indexOf(T a) {
+        for (int i=0; i<size; i++) {
+            if(arr[i] == a) return i;
+        }
+        return -1;
     }
 
     public void writeList() {
         for (T v : arr)
             System.out.print("["+v+"] ");
+        System.out.println("size="+size);
     }
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,9 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         ArrList<String> arr = new ArrList<>(10);
-        arr.add("asdf");
-        System.out.println(arr.toString());
-        //System.out.println(arr[0]);
+        arr.add("aaa");
+        arr.add("bbb", 1);
+        arr.add("ccc", 5);
+        arr.add("ddd", 11);
         arr.writeList();
+        arr.remove(5);
+        arr.writeList();
+        System.out.println("indexOf('bbb') = " + arr.indexOf("bbb"));
+        System.out.println("indexOf('asdf') = " + arr.indexOf("asdf"));
+
+
     }
 }
